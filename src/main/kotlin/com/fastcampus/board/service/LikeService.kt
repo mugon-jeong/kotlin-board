bdb44eb0f4f2ef9adcf460f4cee6cf0413ce7dbb
jmg <jmg41490400@gmail.com>
2023-10-09 16:36:49 +0900

    feat: Add like function

---
+++ src/main/kotlin/com/fastcampus/board/service/LikeService.kt
@@ -0,0 +1,22 @@
+package com.fastcampus.board.service
+
+import com.fastcampus.board.domain.Like
+import com.fastcampus.board.exception.PostNotFoundException
+import com.fastcampus.board.repository.LikeRepository
+import com.fastcampus.board.repository.PostRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class LikeService(
+    private val likeRepository: LikeRepository,
+    private val postRepository: PostRepository,
+) {
+    @Transactional
+    fun createLike(postId: Long, createdBy: String): Long {
+        val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException()
+        return likeRepository.save(Like(post, createdBy)).id
+    }
+}
---
+++ src/test/kotlin/com/fastcampus/board/service/LikeServiceTest.kt
@@ -0,0 +1,38 @@
+package com.fastcampus.board.service
+
+import com.fastcampus.board.domain.Post
+import com.fastcampus.board.exception.PostNotFoundException
+import com.fastcampus.board.repository.LikeRepository
+import com.fastcampus.board.repository.PostRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+class LikeServiceTest(
+    private val likeService: LikeService,
+    private val likeRepository: LikeRepository,
+    private val postRepository: PostRepository,
+) : BehaviorSpec({
+    given("좋아요 생성시") {
+        val saved = postRepository.save(Post("harris", "title", "content"))
+        When("인풋이 정상적으로 들어오면") {
+            val likeId = likeService.createLike(saved.id, "harris")
+            then("좋아요가 정상적으로 생성됨을 확인한다.") {
+                val like = likeRepository.findByIdOrNull(likeId)
+                like shouldNotBe null
+                like?.createdBy shouldBe "harris"
+            }
+        }
+        When("게시글이 존재하지 않으면") {
+            then("존재하지 앟는 게시글 예외가 발생한다.") {
+                shouldThrow<PostNotFoundException> {
+                    likeService.createLike(9999L, "harris")
+                }
+            }
+        }
+    }
+})
