e8194105d30c069ca618551e79403441098e8391
jmg <jmg41490400@gmail.com>
2023-10-09 18:23:01 +0900

    feat: Create index

--- src/main/kotlin/com/fastcampus/board/domain/Comment.kt
+++ src/main/kotlin/com/fastcampus/board/domain/Comment.kt
@@ -2,11 +2,14 @@ package com.fastcampus.board.domain
 
 import com.fastcampus.board.exception.CommentNotUpdatableException
 import com.fastcampus.board.service.dto.CommentUpdateRequestDto
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
 @Entity
@@ -29,6 +32,7 @@ class Comment(
         protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
     var post: Post = post
         protected set
 }
--- src/main/kotlin/com/fastcampus/board/repository/TagRepository.kt
+++ src/main/kotlin/com/fastcampus/board/repository/TagRepository.kt
@@ -1,5 +1,6 @@
 package com.fastcampus.board.repository
 
+import com.fastcampus.board.domain.QPost.post
 import com.fastcampus.board.domain.QTag.tag
 import com.fastcampus.board.domain.Tag
 import org.springframework.data.domain.Page
@@ -19,7 +20,9 @@ interface CustomTagRepository {
 class CustomTagRepositoryImpl : CustomTagRepository, QuerydslRepositorySupport(Tag::class.java) {
     override fun findPageBy(pageRequest: Pageable, tagName: String): Page<Tag> {
         return from(tag)
-            .join(tag.post).fetchJoin() // n+1 쿼리 해결(tag조회시 post 함께 조회)
+            // order by의 tag.post와 join의 tag.post가 매핑이 안될때 join문이 2개 생성됨
+            // join에 2번째 인자로 post 넣어 매핑해줘야함
+            .join(tag.post, post).fetchJoin() // n+1 쿼리 해결(tag조회시 post 함께 조회)
             .where(tag.name.eq(tagName))
             .orderBy(tag.post.createdAt.desc())
             .offset(pageRequest.offset)
